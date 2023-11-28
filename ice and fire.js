917cac9f4f79f7acd9a9916f080b6d511d420fef
Vijay N <120193561+VIJAY7043@users.noreply.github.com>
2023-11-28 03:16:23 -0800

    Update ice and fire.js

--- ice and fire.js
+++ ice and fire.js
@@ -1,32 +1,59 @@
-let API = "https://anapioficeandfire.com/api/characters/583"
-console.log(API)
 
-function ReadAllData(){
-    fetch(API,{
-        method : "GET"
-    })
-    .then((response)=>response.json())
-    .then(((data)=>console.log(data)))
-    .catch((err)=>console.log(err))
-}  
-ReadAllData();
-//operation in html
-const booklist = document.querySelector("#book-list")
-function renderbook(book){
-    const bookdiv = document.createElement("div")
-    bookdiv.className="card"
-    bookdiv.innerHTML +=
-    <h2>book</h2>
-    <P><a href="https://anapioficeandfire.com/api/books/5"></a></P>
+document.addEventListener('DOMContentLoaded', () => {
+    
+});
 
-booklist.appendChild(bookdiv)
-}
-renderbook(newbookdata)
-// render all the books
-function renderallbooks(books){
-    booklist.innerHTML=" ";
-    book.foreach((book)=>{
-        book
-    })
-} 
-renderallbooks()
+document.addEventListener('DOMContentLoaded', async () => {
+    const apiUrl = 'https://anapioficeandfire.com/api/books';
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+  
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+});
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const apiUrl = 'https://anapioficeandfire.com/api/books';
+    const booksContainer = document.getElementById('books-container');
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        data.slice(0, 10).forEach(book => {
+            const bookElement = document.createElement('div');
+            bookElement.innerHTML = `
+                <h2>${book.name}</h2>
+                <p><strong>ISBN:</strong> ${book.isbn}</p>
+                <p><strong>Pages:</strong> ${book.numberOfPages}</p>
+                <p><strong>Authors:</strong> ${book.authors.join(', ')}</p>
+                <p><strong>Publisher:</strong> ${book.publisher}</p>
+                <p><strong>Released Date:</strong> ${book.released}</p>
+            `;
+
+            booksContainer.appendChild(bookElement);
+        });
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+});
+
+document.addEventListener('DOMContentLoaded', async () => {
+   
+
+    const searchInput = document.createElement('input');
+    searchInput.placeholder = 'Search books';
+
+    searchInput.addEventListener('input', () => {
+        const searchTerm = searchInput.value.toLowerCase();
+
+        const filteredBooks = data.filter(book => book.name.toLowerCase().includes(searchTerm));
+
+    });
+
+    document.body.insertBefore(searchInput, booksContainer);
+});
